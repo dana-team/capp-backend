b814e8822b8c9e5fcda87a48b4bfc8dcf8cc6d67
maromcohen <maromcohen@dana.com>
2026-04-19 13:41:40 +0300

    feat: Added option to create namespace that will have the label 'dana.io/capp-ns:true

--- internal/resources/consts/consts.go
+++ internal/resources/consts/consts.go
@@ -1,5 +1,8 @@
 package consts
 
 const (
-	ManagedLabelKey = "dana.io/capp-managed"
+	ManagedLabelKey          = "dana.io/capp-managed"
+	ManagedNameSpaceLabelKey = "dana.io/capp-ns"
+	CappAPIGroup             = "rcs.dana.io"
+	CappResource             = "capps"
 )
